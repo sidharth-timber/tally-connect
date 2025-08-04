8604166b3e39d57867e033138e2395753c8113f6
Sidharth PT <sidharthanand73@gmail.com>
2025-08-04 15:03:16 +0530

    fix: refactoring

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 
-
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
